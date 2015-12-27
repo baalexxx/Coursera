7e04367ec08dcb54a2860044f925f59ad50167be
baal <baal@belightsoft.com>
2015-12-27 19:34:03 +0100

    Created Sample project

---
+++ sample/sample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sample
+//
+//  Created by Alex Baev on 27/12/15.
+//  Copyright © 2015 baevsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

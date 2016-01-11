9e2c1e9d76c7e4ca0b75b9fc4c81b712635b349b
baal <baal@belightsoft.com>
2016-01-11 08:34:12 +0100

    Done

--- PeerReview03/PeerReview03/ViewController.m
+++ PeerReview03/PeerReview03/ViewController.m
@@ -8,7 +8,21 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
+
+
+
+@interface ViewController () <CRCurrencyRequestDelegate>
+
+
+@property (nonatomic) CRCurrencyRequest* req;
+
+@property (weak, nonatomic) IBOutlet UITextField *unputField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *currencySegment;
+
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
+@property (weak, nonatomic) IBOutlet UILabel *currencyLabel;
 
 @end
 
@@ -22,6 +36,56 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies {
+    
+    if (self.currencySegment.selectedSegmentIndex == 0) {
+        
+       float eur = [self.unputField.text floatValue] * currencies.EUR;
+        
+        NSString* temp = [NSString stringWithFormat:@"%2.f", eur];
+        
+        
+        self.currencyLabel.text = temp;
+    }
+    else if (self.currencySegment.selectedSegmentIndex == 1) {
+        
+        float yen = [self.unputField.text floatValue] * currencies.JPY;
+        
+        NSString* temp = [NSString stringWithFormat:@"%2.f", yen];
+        
+        
+        self.currencyLabel.text = temp;
+    }
+    else if (self.currencySegment.selectedSegmentIndex == 2) {
+        
+        float gbp = [self.unputField.text floatValue] * currencies.GBP;
+        
+        NSString* temp = [NSString stringWithFormat:@"%2.f", gbp];
+        
+        
+        self.currencyLabel.text = temp;
+    }
+    
+    self.convertButton.enabled = YES;
+    
+    
+}
+
+- (IBAction)buttonTapped:(id)sender {
+    
+    self.convertButton.enabled = NO;
+    
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    
+    [self.req start];
+    
+    
+    
+    
 }
 
 @end

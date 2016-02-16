227dade12c573514565a8b197bf51b379aade1f3
baal <baal@belightsoft.com>
2016-02-16 22:45:57 +0100

    Create Profect TwitterShare

---
+++ TwitterShare/TwitterShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterShare
+//
+//  Created by Alex Baev on 16/02/16.
+//  Copyright © 2016 baevsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TwitterShare/TwitterShare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwitterShare
+//
+//  Created by Alex Baev on 16/02/16.
+//  Copyright © 2016 baevsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
